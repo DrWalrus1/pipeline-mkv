73eafe65397a700190f2dd84f2819409ec5b2a66
Luke Lynch <luke@lukelynch.io>
2025-05-13 23:21:43 +1000

    Adding css to preview. Adding RipDisc component

---
+++ frontend/src/components/RipDisc.stories.ts
@@ -0,0 +1,12 @@
+import type { Meta, StoryObj } from "@storybook/vue3";
+import RipDisc from "./RipDisc.vue";
+
+const meta: Meta<typeof RipDisc> = {
+  component: RipDisc,
+}
+
+export default meta;
+type Story = StoryObj<typeof RipDisc>;
+
+export const Primary: Story = {
+};
---
+++ frontend/src/components/RipDisc.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <select>
+    <option value="disc:1">Disc 1</option>
+    <option value="disc:2">Disc 2</option>
+    <option value="disc:3">Disc 3</option>
+  </select>
+  <br>
+  <input type="checkbox" name="title" value="0">Title 0</input>
+  <br>
+  <input type="checkbox" name="title" value="1">Title 1</input>
+  <br>
+  <input type="checkbox" name="title" value="2">Title 2</input>
+  <br>
+  <input type="checkbox" name="title" value="3">Title 3</input>
+  <br>
+  <input type="checkbox" name="title" value="4">Title 4</input>
+  <br>
+  <Button>Rip Disc</Button>
+</template>
